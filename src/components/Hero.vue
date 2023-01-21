74a823077bf67cc8105ff5517e114eb61e29b16e
yahyacodes <yahya.codin@gmail.com>
2023-01-21 20:36:36 +0300

    m

---
+++ src/components/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hero-style">
+    <h1 class="text-3xl font-bold underline">Sripe clone Project Under-way</h1>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+.hero-style {
+  background-color: hsla(62, 100%, 48%, 1);
+  background-image: radial-gradient(
+      at 87% 9%,
+      hsla(260, 100%, 74%, 1) 0px,
+      transparent 50%
+    ),
+    radial-gradient(at 9% 41%, hsla(189, 92%, 44%, 1) 0px, transparent 50%),
+    radial-gradient(at 0% 66%, hsla(61, 100%, 50%, 1) 0px, transparent 50%),
+    radial-gradient(at 95% 27%, hsla(51, 100%, 50%, 1) 0px, transparent 50%),
+    radial-gradient(at 0% 97%, hsla(3, 100%, 50%, 1) 0px, transparent 50%),
+    radial-gradient(at 91% 75%, hsla(264, 100%, 50%, 1) 0px, transparent 50%),
+    radial-gradient(at 0% 0%, hsla(287, 100%, 50%, 1) 0px, transparent 50%);
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="container m-auto">
+    <nav class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 bg-gray-900">
+      <div
+        class="container flex flex-wrap items-center justify-between mx-auto"
+      >
+        <span class="self-center text-xl font-semibold whitespace-nowrap"
+          >Stripe</span
+        >
+        <div class="flex md:order-2">
+          <button
+            type="button"
+            class="text-white bg-gray-50 hover:bg-gray-100 rounded-full text-sm px-5 py-2 text-center mr-3 md:mr-0"
+          >
+            Sign in
+          </button>
+          <button
+            data-collapse-toggle="navbar-sticky"
+            type="button"
+            class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-sticky"
+            aria-expanded="false"
+          >
+            <span class="sr-only">Open main menu</span>
+            <svg
+              class="w-6 h-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <div
+          class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          id="navbar-sticky"
+        >
+          <ul
+            class="flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0"
+          >
+            <li>
+              <a
+                href="#"
+                class="group block py-2 pl-3 pr-4 text-gray-100 md:p-0"
+                >Home</a
+              >
+              <div
+                class="z-10 hidden group-hover:block font-normal bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+              >
+                <ul
+                  class="py-1 text-sm text-gray-700 dark:text-gray-400"
+                  aria-labelledby="dropdownLargeButton"
+                >
+                  <li>
+                    <a
+                      href="#"
+                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >Dashboard</a
+                    >
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >Settings</a
+                    >
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >Earnings</a
+                    >
+                  </li>
+                </ul>
+                <div class="py-1">
+                  <a
+                    href="#"
+                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
+                    >Sign out</a
+                  >
+                </div>
+              </div>
+            </li>
+
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 text-gray-100 md:p-0"
+                >About</a
+              >
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 text-gray-100 md:p-0"
+                >Services</a
+              >
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 text-gray-100 md:p-0"
+                >Contact</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
